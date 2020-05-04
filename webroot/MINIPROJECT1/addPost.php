dc1deba78b459bd9e89fde5303967bc656c93941
Melisa Bogdan <melisa.bogdan@gmail.com>
2020-05-04 23:59:58 +0100

    test4

--- webroot/MINIPROJECT1/addPost.php
+++ webroot/MINIPROJECT1/addPost.php
@@ -1,10 +1,12 @@
 <?php
 	session_start();
-	$user ='root';
-	$pass = '';
-	$db= 'testdb';
-	// connection to XAMPP database
-	$conn= new mysqli('localhost', $user,$pass,$db,"3306") or die("unable to connect oof");
+	$dbhost = getenv("MYSQL_SERVICE_HOST"); 
+	$dbport = getenv("MYSQL_SERVICE_PORT"); 
+	$dbuser = getenv("DATABASE_USER"); 
+	$dbpwd = getenv("DATABASE_PASSWORD"); 
+	$dbname = getenv("DATABASE_NAME"); 
+	// Create connection 
+	$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname) or die("unable to connect oof"); 
 	
 	$title = $_POST['title']; 
 	$date =date('Y-m-d H:i:s');
