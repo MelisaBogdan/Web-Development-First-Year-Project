71435b1ecb7f4c16a5fe4d75dc1ca95478fc1b3c
Melisa Bogdan <melisa.bogdan@gmail.com>
2020-05-05 13:16:20 +0100

    aaaaaa

--- webroot/MINIPROJECT1/index.php
+++ webroot/MINIPROJECT1/index.php
@@ -13,10 +13,8 @@ session_start();
 	#if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	#{ 
 		#This is to ensure that the INSERT query does not run EVERY TIME the page is loaded, even if no form was submitted.     
-		$sql = "INSERT INTO testt(ID,User,Pass)  #create the row
-		VALUES('1',' ',' ')"; 
-		$sql = "UPDATE testt SET User='$userr', Pass='$passs'  
-			WHERE ID=1";    
+		$sql = "INSERT INTO testt(ID,User,Pass) VALUES('1',' ',' ')"; 
+		$sql = "UPDATE testt SET User='$userr', Pass='$passs'  WHERE ID=1";    
 	#}
 	if ($conn->query($sql) === TRUE) 
 	{               
